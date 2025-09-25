79fd13ca16239cc7a9155a434dc64a19de499ad6
danX-glitch <shtepenko.daniil@gmail.com>
2025-09-25 09:18:58 +0500

    начало внедрения погодной API

---
+++ app/src/main/java/com/nearby/weatherdaniil/data/remote/WeatherApiService.kt
@@ -0,0 +1,17 @@
+package com.nearby.weatherdaniil.data.remote
+
+import com.nearby.weatherdaniil.data.model.WeatherResponce
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WeatherApiService {
+    @GET("forecast")
+    suspend fun getForecast(
+        @Query("latitude") latitude: Double,
+        @Query("longitude") longitude: Double,
+        @Query("hourly") hourly: String = "temperature_2m,relative_humidity_2m,weather_code,rain",
+        @Query("wind_speed_unit") windSpeedUnit: String = "ms"
+
+    ): WeatherResponce
+
+}
